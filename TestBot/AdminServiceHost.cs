3d3527f4d948d36adf230cbf5a1193f299ed3e20
bb <bba2@yandex.ru>
2020-06-19 19:17:16 +1000

    Revert "."
    This reverts commit 12742f06e8186b23a295fd0f289f30199899b880.

--- TestBot/AdminServiceHost.cs
+++ TestBot/AdminServiceHost.cs
@@ -5,45 +5,19 @@ using System.ServiceModel;
 using System.ServiceModel.Description;
 using System.ServiceModel.Web;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestBot
 {
     internal class AdminServiceHost
     {
-        private Thread th = null;
-        private WebServiceHost host = null;
-        private ServiceEndpoint ep = null;
-        private ServiceDebugBehavior sdb = null;
-
-        private object lckObj = new object();
-        private bool lckObjFlg = false;
-
         internal AdminServiceHost()
         {
-            host.Open();
-        }
-
-        internal void Prepare(string httpAdr)   // "http://localhost:8000/"
-        {
-            host = new WebServiceHost(typeof(AdminService), new Uri(httpAdr));
-            ep = host.AddServiceEndpoint(typeof(IAdminService), new WebHttpBinding(), "");
-            sdb = host.Description.Behaviors.Find<ServiceDebugBehavior>();
+            WebServiceHost host = new WebServiceHost(typeof(AdminService), new Uri("http://localhost:8000/"));
+            ServiceEndpoint ep = host.AddServiceEndpoint(typeof(IAdminService), new WebHttpBinding(), "");
+            ServiceDebugBehavior sdb = host.Description.Behaviors.Find<ServiceDebugBehavior>();
             sdb.HttpHelpPageEnabled = false;
-        }
-
-        internal void Open()
-        {
-            th = new Thread(() => 
-                                {
-                                    Monitor.Enter(lckObj, ref lckObjFlg);
-                                    if (lckObjFlg)
-                                    {
-                                        host.Open();
-                                        Monitor.Wait(lckObj);
-                                    }
-                                });
+            host.Open();
         }
     }
 }
